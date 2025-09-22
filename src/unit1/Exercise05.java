aff2295785f35279199d35f58623852d848e3383
mario <mario@Mario>
2025-09-22 17:33:09 +0200

    22/09

--- src/unit1/Exercise05.java
+++ src/unit1/Exercise05.java
@@ -6,20 +6,20 @@ public class Exercise05 {
 	public static void main(String[] args) {
 		// Creation of the scanner
 		Scanner sc = new Scanner(System.in);
-		// Declaration of variables
+		// Declaration of variable to ask the radius
 		double radius;
-		double area;
-		double length;
 		// Ask the user for the radius
 		System.out.println("Introduce el radio de tu circunferencia: ");
 		// Read the console input of the radius
 		radius = sc.nextDouble();
 		// Close the scanner
 		sc.close();
-		// Calculate the area
-		area = (2*Math.PI)*(radius*radius);
-		// Show the results
-		System.out.println("El área de tu circunferencia es: " + area);
 		// Calculate the length
+		double length = 2 * Math.PI * radius;
+		// Calculate the area
+		double area = Math.PI * Math.pow(radius, 2);
+		// Show the results
+		System.out.println("El área de tu circunferencia es: " + area );
+		System.out.println("La longitud de tu circunferencia es: " + length );
 	}
 }
