fcd1e990db1663bb77a3903f75ff1d3013455c56
mario.laso <mario.laso@106-12.info2>
2025-09-29 09:15:10 +0200

    29/09

--- src/part2/Exercise01.java
+++ src/part2/Exercise01.java
@@ -1,7 +1,22 @@
 package part2;
 
+import java.util.Scanner;
+
 public class Exercise01 {
 	public static void main(String[] args) {
+		// Declare variable to storage the number
+		double number;
+		// Declare variable to storage the rounded number
+		double rounded;
+		// Create the scanner
+		Scanner reader = new Scanner(System.in);
+		// Ask the user for the number
+		System.out.println("Introduce el número a redondear: ");
+		// Scan the input
+		number = reader.nextDouble();
+		// 
+		
+		
 		
 	}
 }
