aa4b702dd9423b1a6226fa464589271ec1837780
mario.laso <mario.laso@106-12.info2>
2025-09-29 10:03:19 +0200

    29/09

--- src/part2/Exercise01.java
+++ src/part2/Exercise01.java
@@ -7,17 +7,17 @@ public class Exercise01 {
 		// Declare variable to storage the number
 		double number;
 		// Declare variable to storage the rounded number
-		double rounded;
+		int rounded;
 		// Create the scanner
 		Scanner reader = new Scanner(System.in);
 		// Ask the user for the number
-		System.out.println("Introduce el número a redondear: ");
+		System.out.println("Introduce el número con decimales a redondear: ");
 		// Scan the input
 		number = reader.nextDouble();
 		// Close the scanner
 		reader.close();
 		// Calculate rounding
-		rounded = (int) number < 5 ? - 1: + 1;
+		rounded = (int) (number + 0.5);
 		// Show the results
 		System.out.println("El número redondeado es: " + rounded);
 	}
