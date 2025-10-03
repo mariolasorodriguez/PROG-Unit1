e9cb0ba4e7c8fead57e4e9b320bde4576f7201b3
mario.laso <mario.laso@106-12.info2>
2025-10-03 12:11:51 +0200

    03/10

---
+++ src/part2/Exercise10.java
@@ -0,0 +1,22 @@
+package part2;
+
+import java.util.Scanner;
+
+public class Exercise10 {
+	public static void main(String[] args) {
+		// Declare variable to store the year
+		int year;
+		// Declare variable to store the century
+		int century;
+		// Create the scanner
+		Scanner reader = new Scanner(System.in);
+		// Ask the user for a year
+		System.out.println("Introduce el año: ");
+		// Scan the input
+		year = reader.nextInt();
+		// Calculate the century
+		century = ((year - 1) / 100) + 1;
+		// Show the result
+		System.out.println("El año: " + year + " pertenece al siglo: " + century);
+	}
+}
