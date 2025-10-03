c0c7230d148e0387821fe696850e4beea3efbabd
mario.laso <mario.laso@106-12.info2>
2025-10-03 12:33:21 +0200

    Realized

--- src/part2/Exercise04.java
+++ src/part2/Exercise04.java
@@ -9,8 +9,9 @@ public class Exercise04 {
 		int a;
 		int b;
 		int c;
+		int x;
 		// Declare variable to show the equation
-		final String y;
+		double y;
 		// Create the scanner
 		Scanner reader = new Scanner(System.in);
 		// Ask the user for the coefficient a
@@ -25,12 +26,16 @@ public class Exercise04 {
 		System.out.println("Introduce el valor de c: ");
 		// Scan the input
 		c = reader.nextInt();
+		// Ask the user for the coefficient x
+		System.out.println("Introduce el valor de x: ");
+		// Scan the input
+		x = reader.nextInt();
 		// Close the scanner
 		reader.close();
 		// Calculate the equation
-		y = a + "x^2+" + b + "x+" + c;
+		y = (a * Math.pow(x, 2)) + (b * x) + c;
 		// Show the equation
-		System.out.println("El valor es igual a y=" + y);
+		System.out.println("El valor es igual a y= " + y);
 	}
 
 }
